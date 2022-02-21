c9b641ecb80fdf6382b69c8c57bdb790e15e5074
FerrorNetwork <40063558+FerrorNetwork@users.noreply.github.com>
2022-02-21 18:32:38 +0400

    added adding posts

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Category;
+
+use App\Post;
+use App\Tag;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+     */
+    public function index()
+    {
+
+        $posts = Post::paginate(10);
+        return view('admin.posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::pluck('title', 'id')->all();
+        $tags = Tag::pluck('title', 'id')->all();
+        return view('admin.posts.create', compact('categories', 'tags'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'content' => 'required',
+            'category_id' => 'integer|required',
+            'thumbnail' => 'nullable|image'
+        ]);
+
+        $data = $request->all();
+
+        if($request->hasFile('thumbnail'))
+        {
+            $folder = date('Y-m-d');
+            $data['thumbnail'] = $request->file('thumbnail')->store("images/{$folder}");
+
+        }
+
+        $post = Post::create($data);
+        $post->tags()->sync($request->tags);
+        $request->session()->flash('success', 'Статья добавлена');
+         return redirect()->route('posts.index');
+    }
+
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+
+        return view('admin.posts.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreCategory $request, $id)
+    {
+        $request->validate([
+
+        ]);
+        $request->session()->flash('success', 'Статья изменена');
+        return redirect()->route('posts.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+
+
+        return redirect()->route('posts.index')->with('success', 'Статья удалена');
+    }
+}
---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,106 @@
+@extends('admin.layouts.layout')
+
+@section('content')
+
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Создание статьи</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+
+                        </ol>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Создание статьи</h3>
+                            </div>
+                            <!-- /.card-header -->
+
+                            <form role="form" method="post" action="{{ route('posts.store') }}" enctype="multipart/form-data">
+                                @csrf
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label for="title">Название статьи</label>
+                                        <input type="text" name="title"
+                                               class="form-control @error('title') is-invalid @enderror" id="title"
+                                               placeholder="Название">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="description">Цитата</label>
+                                        <textarea name="description" class="form-control" id="description" rows="5" placeholder="Цитата ..."></textarea>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="content">Контент</label>
+                                        <textarea name="content" class="form-control" id="content" rows="7" placeholder="Контент ..."></textarea>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="category_id">Категория</label>
+
+                                        <select class="form-control" id="category_id" name="category_id">
+
+                                            @foreach($categories as $k => $v)
+                                            <option value="{{$k}}">{{ $v }}</option>
+                                            @endforeach
+
+                                        </select>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="tags">Теги</label>
+                                        <select  name="tags[]" id="tags" class="select" multiple="multiple" data-placeholder="Выбор тегов" style="width: 100%;">
+                                            @foreach($tags as $k => $v)
+                                            <option value="{{$k}}">{{$v}}</option>
+
+                                            @endforeach
+                                        </select>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="thumbnail">Изображение</label>
+                                        <div class="input-group">
+                                            <div class="custom-file">
+                                                <input type="file" name="thumbnail" id="thumbnail" class="custom-file-input" >
+                                                <label class="custom-file-label" for="thumbnail">Choose file</label>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- /.card-body -->
+
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-primary">Сохранить</button>
+                                </div>
+                            </form>
+
+                        </div>
+                        <!-- /.card -->
+
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </div>
+
+
+        </section>
+        <!-- /.content -->
+
+@endsection
+
